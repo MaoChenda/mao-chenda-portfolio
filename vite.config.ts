19cff8758ec3b1681606af017b5f4646475277ba
MaoChenda <mao.chenda60@gmail.com>
2026-08-18 11:28:26 +0700

    vite base

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import vue from '@vitejs/plugin-vue'
 // If deploying to GitHub Pages at https://<user>.github.io/<repo>/,
 // set base to '/<repo>/'. For a root domain or Netlify/Vercel, leave as '/'.
 export default defineConfig({
-  base: '/',
+  base: '/mao-chenda-portfolio/',
   plugins: [vue()],
 })
